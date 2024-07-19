f236ea26ade268efca52d36d10e178abed7df203
Swapnil <swapnilchand51@gmail.com>
2024-07-20 03:50:37 +0530

    Linkedin and about section

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
+    const aboutToggle = document.getElementById('aboutToggle');
+    const aboutSection = document.getElementById('aboutSection');
+    const container = document.querySelector('.container');
 
     menuToggle.addEventListener('click', () => {
         navLinks.classList.toggle('active');
+    });
+    aboutToggle.addEventListener('click', function(e) {
+        e.preventDefault();
+        aboutSection.classList.toggle('open');
+        container.classList.toggle('shift');
     });
 });
 
