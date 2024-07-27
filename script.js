f5a53b708933cf17ab452f2bdbf94b353f2e53f7
Swapnil <swapnilchand51@gmail.com>
2024-07-27 13:14:42 +0530

    qowidj'

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
-    const aboutToggle = document.getElementById('aboutToggle');
-    const aboutSection = document.getElementById('aboutSection');
-    const container = document.querySelector('.container');
 
     menuToggle.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
-    aboutToggle.addEventListener('click', function(e) {
-        e.preventDefault();
-        aboutSection.classList.toggle('open');
-        container.classList.toggle('shift');
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    const scrollToTopButton = document.getElementById('scroll-to-top');
+
+    const toggleScrollToTopButton = () => {
+        if (window.scrollY > 300) {
+            scrollToTopButton.classList.add('visible');
+        } else {
+            scrollToTopButton.classList.remove('visible');
+        }
+    };
+
+    window.addEventListener('scroll', toggleScrollToTopButton);
+
+    scrollToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
     });
 });
-
